76cc69bd488893a42cf3012fd3a9c0b37ea2747d
Richard-Devine <richard.devine@inscope.sh>
2024-10-13 15:51:31 +0100

    fix: Name fix

--- src/components/widget-component-wrapper/WidgetComponentWrapper.test.tsx
+++ src/components/widget-component-wrapper/WidgetComponentWrapper.test.tsx
@@ -1,9 +1,9 @@
 import {render, screen} from "@testing-library/react";
-import WidgeComponentWrapper from "./WidgetComponentWrapper.tsx";
+import WidgetComponentWrapper from "./WidgetComponentWrapper.tsx";
 
 const titles = ['Test title 1', 'Test title 2', ''];
 test.each(titles)('Title text exist', (title) => {
-    render(<WidgeComponentWrapper title={title}><></></WidgeComponentWrapper>)
+    render(<WidgetComponentWrapper title={title}><></></WidgetComponentWrapper>)
     const titleComponent = screen.getByTestId('widget-component-wrapper-title')
     expect(titleComponent).toHaveTextContent(title)
 });
