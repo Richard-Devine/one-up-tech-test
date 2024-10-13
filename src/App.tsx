caf48fd2a8a2ebfc0536b6f05e6c727722a3633c
Richard-Devine <richard.devine@inscope.sh>
2024-10-13 20:06:51 +0100

    feat: App build

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,155 @@ import './App.css'
 import Widget from "./components/widget/Widget.tsx";
 import DoughnutChart from "./components/doughnut-chart/DoughnutChart.tsx";
 import WidgetComponentWrapper from "./components/widget-component-wrapper/WidgetComponentWrapper.tsx";
+import Input from "./components/input/Input.tsx";
+import {useCallback, useEffect, useState} from "react";
+import calculateCommission from "./mockApi.ts";
+
+interface CommissionBand {
+    rate: number,
+    commission: number,
+    threshold: number,
+    title: string
+}
+
+interface Commission {
+    [key: string]: CommissionBand;
+}
 
 function App() {
+
+    const [testValue, setTestValue] = useState('')
+    const [commissionRatesAndValues, setCommissionRatesAndValues] = useState<Commission | null>(null)
+    const {bandOne, bandTwo, bandThree, bandFour, bandFive} = commissionRatesAndValues ?? {}
+    const testValueToPence = (parseFloat(testValue) * 100 | 0)
+
+    useEffect(() => {
+        const commission = calculateCommission(0)
+        setCommissionRatesAndValues(commission)
+    }, []);
+
+    const onChangeHandler = (value: string) => {
+        const valueInPence = Number(parseFloat(value) * 100)
+        setTestValue(valueInPence.toString())
+        const commission = calculateCommission(valueInPence)
+        setCommissionRatesAndValues(commission)
+    }
+
+    const getPercentage = (partNumber: number, wholeNumber: number) => {
+        return Math.floor(partNumber / wholeNumber * 100);
+    }
+
+    const penceToPounds = (pence: number) => {
+        return (pence / 100).toFixed(2);
+    }
+    const amountToUnlock = (threshold: number) => {
+        return penceToPounds(threshold - testValueToPence)
+    }
+
+    const calculateTotalCommission = useCallback(() => {
+        let total = 0
+        if (commissionRatesAndValues) {
+            for (const key of Object.keys(commissionRatesAndValues)) {
+                total += commissionRatesAndValues[key].commission
+            }
+        }
+        return total
+    }, [commissionRatesAndValues])
+
+    const totalCommission = penceToPounds(calculateTotalCommission())
 
     return (
         <>
             <Widget header={'Commission Breakdown'}>
-                <div className={'commission-totals'}>
-                    <WidgetComponentWrapper title='Total Revenue'>
-                        <p>£56324</p>
-                    </WidgetComponentWrapper>
-                    <WidgetComponentWrapper title={'Total Commission'}>
-                        <p>£56324</p>
-                    </WidgetComponentWrapper>
-                </div>
-                <WidgetComponentWrapper title={'£0-£5000'} >
-                    <DoughnutChart fillPercent={100} fillPercentText
-                                   centerText={'Commission: £0'}></DoughnutChart>
-                </WidgetComponentWrapper>
-                <WidgetComponentWrapper title={'£5000-£10000'}>
-                    <DoughnutChart  fillPercent={100} fillPercentText
-                                   centerText={'Commission: £200'}></DoughnutChart>
-                </WidgetComponentWrapper>
-                <WidgetComponentWrapper title={'£10000-£15000'} >
-                    <DoughnutChart fillPercent={58} fillPercentText
-                                   centerText={'Center text'}></DoughnutChart>
-                </WidgetComponentWrapper>
-                <WidgetComponentWrapper title={'£15000-£20000'} >
-                    <DoughnutChart fillPercent={0}
-                                   centerText={'£4632 to unlock band 4'}></DoughnutChart>
-                </WidgetComponentWrapper>
-                <WidgetComponentWrapper title={'£20000+'} >
-                    <DoughnutChart fillPercent={0} fillPercentText
-                                   centerText={'Center text'}></DoughnutChart>
-                </WidgetComponentWrapper>
+                {!commissionRatesAndValues ? <>Loading...</> :
+                    <>
+                        <div className={'commission-totals'}>
+                            <WidgetComponentWrapper title='Revenue'>
+                                <Input
+                                    placeholder='Enter amount (£)'
+                                    value={testValue}
+                                    onChangeHandler={(value) => onChangeHandler(value)}/>
+                            </WidgetComponentWrapper>
+                            <WidgetComponentWrapper title={'Total Commission'}>
+                                <p>£{totalCommission}</p>
+                            </WidgetComponentWrapper>
+                        </div>
+
+                        <WidgetComponentWrapper title={`${bandOne.title} - ${bandOne.rate}%`}>
+                            <DoughnutChart
+                                fillPercent={
+                                    testValueToPence >= bandOne.threshold ?
+                                        100 :
+                                        getPercentage(testValueToPence, bandOne.threshold)
+                                }
+                                fillPercentText
+                                centerText={
+                                    testValueToPence >= bandOne.threshold ?
+                                        `Commission: £${penceToPounds(bandOne.commission)}` :
+                                        `£${amountToUnlock(bandOne.threshold)} to unlock`}>
+                            </DoughnutChart>
+                        </WidgetComponentWrapper>
+
+                        <WidgetComponentWrapper title={`${bandTwo.title} - ${bandTwo.rate}%`}>
+                            <DoughnutChart
+                                fillPercent={
+                                    testValueToPence >= bandTwo.threshold ?
+                                        100 :
+                                        getPercentage(testValueToPence, bandTwo.threshold)
+                                }
+                                fillPercentText
+                                centerText={
+                                    testValueToPence >= bandTwo.threshold ?
+                                        `Commission: £${penceToPounds(bandTwo.commission)}` :
+                                        `£${amountToUnlock(bandTwo.threshold)} to unlock`}>
+                            </DoughnutChart>
+                        </WidgetComponentWrapper>
+
+                        <WidgetComponentWrapper title={`${bandThree.title} - ${bandThree.rate}%`}>
+                            <DoughnutChart
+                                fillPercent={
+                                    testValueToPence >= bandThree.threshold ?
+                                        100 :
+                                        getPercentage(testValueToPence, bandThree.threshold)
+                                }
+                                fillPercentText
+                                centerText={
+                                    testValueToPence >= bandThree.threshold ?
+                                        `Commission: £${penceToPounds(bandThree.commission)}` :
+                                        `£${amountToUnlock(bandThree.threshold)} to unlock`}>
+                            </DoughnutChart>
+                        </WidgetComponentWrapper>
+
+                        <WidgetComponentWrapper title={`${bandFour.title} - ${bandFour.rate}%`}>
+                            <DoughnutChart
+                                fillPercent={
+                                    testValueToPence >= bandFour.threshold ?
+                                        100 :
+                                        getPercentage(testValueToPence, bandFour.threshold)
+                                }
+                                fillPercentText
+                                centerText={
+                                    testValueToPence >= bandFour.threshold ?
+                                        `Commission: £${penceToPounds(bandFour.commission)}` :
+                                        `£${amountToUnlock(bandFour.threshold)} to unlock`}>
+                            </DoughnutChart>
+                        </WidgetComponentWrapper>
+
+                        <WidgetComponentWrapper title={`${bandFive.title} - ${bandFive.rate}%`}>
+                            <DoughnutChart
+                                fillPercent={
+                                    testValueToPence >= bandFive.threshold ?
+                                        100 :
+                                        getPercentage(testValueToPence, bandFive.threshold)
+                                }
+                                fillPercentText
+                                centerText={
+                                    testValueToPence >= bandFive.threshold ?
+                                        `Commission: £${penceToPounds(bandFive.commission)}` :
+                                        `£${amountToUnlock(bandFive.threshold)} to unlock`}>
+                            </DoughnutChart>
+                        </WidgetComponentWrapper></>
+                }
             </Widget>
         </>
     )
